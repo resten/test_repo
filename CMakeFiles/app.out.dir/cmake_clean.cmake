25154368c38c0ff80f587fa71dad7d6a45c4f9bd
byeongho.park <byeongho.park@lge.com>
2018-08-23 09:01:35 +0900

    cmake mod

---
+++ CMakeFiles/app.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/app.out.dir/hw.c.o"
+  "app.out.pdb"
+  "app.out"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/app.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
